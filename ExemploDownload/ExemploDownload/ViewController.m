ca4a0f16d4d8937cff3f8d7fb35170f070ac48a9
André Cocuroci <andrec@gazetadopovo.com.br>
2013-05-03 16:59:43 -0300

    Exemplo 1 cap 6

--- ExemploDownload/ExemploDownload/ViewController.m
+++ ExemploDownload/ExemploDownload/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFHTTPRequestOperation.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,57 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+-(IBAction)startDownload:(id)sender
+{
+    NSURL *url = [NSURL URLWithString:downloadField.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSString *saveFilename = [self downloadSavePathFor:url.lastPathComponent];
+    
+    NSLog(@"Salvando o arquivo em: %@", saveFilename);
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.outputStream = [NSOutputStream outputStreamToFileAtPath:saveFilename append:YES];
+    
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [loadingIndicator stopAnimating];
+        loadingIndicator.hidden = YES;
+        
+        [self showMessage:@"Download finalizado com sucesso"];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [loadingIndicator stopAnimating];
+        loadingIndicator.hidden = YES;
+        [self showMessage:[NSString stringWithFormat:@"Erro no download: %@", [error localizedDescription]]];
+    }];
+    
+    [operation setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
+        progressBar.progress = (float)totalBytesRead / (float)totalBytesExpectedToRead;
+    }];
+    
+    progressBar.hidden = NO;
+    loadingIndicator.hidden = NO;
+    [loadingIndicator startAnimating];
+    
+    [operation start];
+}
+
+-(NSString *)downloadSavePathFor:(NSString *)filename
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0];
+    return [documentsPath stringByAppendingPathComponent:filename];
+}
+
+-(void)showMessage:(NSString *)message
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Aviso" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
